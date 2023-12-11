ffcebee4b3185a21cf2144fd198454d3b463d97d
Paulpey13 <paul.peyssard@gmail.com>
2023-12-11 21:01:56 +0100

    modif

--- baseline_transformer_POS_LEM_MORPH.py
+++ baseline_transformer_POS_LEM_MORPH.py
@@ -61,11 +61,6 @@ class PositionalEncoding(nn.Module):
         position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
         pe = torch.zeros(max_len, 1, d_model)
-        # pe[:, 0::2] = torch.sin(position * div_term)
-        # pe[:, 1::2] = torch.cos(position * div_term)
-        # pe = pe.unsqueeze(0).transpose(0, 1)
-        # self.register_buffer('pe', pe)
-        
 
         # before merging morph :
         pe[:, 0, 0::2] = torch.sin(position * div_term)
