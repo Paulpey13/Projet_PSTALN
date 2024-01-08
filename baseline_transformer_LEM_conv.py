c760afdd19d67ad55f1186a7d4e3673665145c85
OXISOGLOU Thomas <thomasoxisoglou@gmail.com>
2024-01-08 16:31:11 +0100

    add LEM test + modif hyper-param

--- baseline_transformer_LEM_conv.py
+++ baseline_transformer_LEM_conv.py
@@ -9,16 +9,16 @@ import math
 
 #import les données
 def load_data(conllu_file):
-    sentences, morph_traits = [], []
+    sentences, lemmas = [], []
     with open(conllu_file, 'r', encoding='utf-8') as file:
         for sentence in parse_incr(file):
             sentences.append([token['form'].lower() for token in sentence])
-            morph_traits.append(['|'.join([f"{k}={v}" for k, v in token['feats'].items()]) if token['feats'] else '_' for token in sentence])
-    return sentences, morph_traits
+            lemmas.append([token['lemma'] for token in sentence])
+    return sentences, lemmas
 
 # Define load_data function
 def load_data_preprocess(conllu_file, char_to_ix, max_word_len):
-    sentences, morph_traits = [], []
+    sentences, lemmas = [], []
     with open(conllu_file, 'r', encoding='utf-8') as file:
         for sentence in parse_incr(file):
             char_sentences = []
@@ -27,33 +27,8 @@ def load_data_preprocess(conllu_file, char_to_ix, max_word_len):
                 chars = chars[:max_word_len] + [char_to_ix['<PAD>']] * (max_word_len - len(chars))
                 char_sentences.append(chars)
             sentences.append(char_sentences)
-            morph_traits.append(['|'.join([f"{k}={v}" for k, v in token['feats'].items()]) if token['feats'] else '_' for token in sentence])
-    return sentences, morph_traits
-
-class MORPHDataset(Dataset):
-    def __init__(self, sentences, morph_traits, morph_to_ix, max_word_len, char_to_ix):
-        # Convert sentences to PyTorch tensors
-        self.sentences = [torch.tensor([self.pad_word(word, max_word_len, char_to_ix['<PAD>']) for word in sentence], dtype=torch.long) for sentence in sentences]
-        
-        self.morph_traits = [torch.tensor([morph_to_ix.get(trait, 0) for trait in trait_list], dtype=torch.long) for trait_list in morph_traits]
-       
-
-    def pad_word(self, word, max_word_len, pad_index):
-        # Function to pad or truncate each word to the same length
-        return word[:max_word_len] + [pad_index] * (max_word_len - len(word))
-    
-    def __len__(self):
-        return len(self.sentences)
-
-    def __getitem__(self, idx):
-        return self.sentences[idx], self.morph_traits[idx]
-
-#Padding
-def collate_fn(batch):
-    sentences, morph_traits = zip(*batch)
-    sentences_padded = pad_sequence(sentences, batch_first=True, padding_value=0)
-    morph_traits_padded = pad_sequence(morph_traits, batch_first=True, padding_value=0)
-    return sentences_padded, morph_traits_padded
+            lemmas.append([token['lemma'] for token in sentence])
+    return sentences, lemmas
 
 class CharCNNEmbedding(nn.Module):
     def __init__(self, num_chars, embedding_dim, num_filters, kernel_size):
@@ -73,69 +48,90 @@ class CharCNNEmbedding(nn.Module):
         x = x.view(batch_size, seq_len, -1)  # Reshape back to [batch_size, seq_len, num_filters]
         return x
 
-#Positional Encoding pour les morphologies 
+class LEMDataset(Dataset):
+    def __init__(self, sentences, lemmas, lemma_to_ix, max_word_len, char_to_ix):
+        # Convert sentences to PyTorch tensors
+        self.sentences = [torch.tensor([self.pad_word(word, max_word_len, char_to_ix['<PAD>']) for word in sentence], dtype=torch.long) for sentence in sentences]
+        
+        self.lemmas = [torch.tensor([lemma_to_ix.get(lemma, 0) for lemma in lemma_list], dtype=torch.long) for lemma_list in lemmas]
+       
+
+    def pad_word(self, word, max_word_len, pad_index):
+        # Function to pad or truncate each word to the same length
+        return word[:max_word_len] + [pad_index] * (max_word_len - len(word))
+    
+    def __len__(self):
+        return len(self.sentences)
+
+    def __getitem__(self, idx):
+        return self.sentences[idx], self.lemmas[idx]
+    
+#Padding
+def collate_fn(batch):
+    sentences, lemmas = zip(*batch)
+    sentences_padded = pad_sequence(sentences, batch_first=True, padding_value=0)
+    lemmas_padded = pad_sequence(lemmas, batch_first=True, padding_value=0)
+    return sentences_padded, lemmas_padded
+
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, dropout=0.1, max_len=5000):
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
-        position = torch.arange(0, max_len).unsqueeze(1)
+
+        position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-        pe = torch.zeros(max_len, d_model)
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
+        x = x + self.pe[:x.size(0)]
         return self.dropout(x)
 
-#Transformer pour morphs seulement
-class MorphTransformerModel(nn.Module):
-    def __init__(self, num_chars, char_embedding_dim, num_filters, kernel_size, nhead, nhid, nlayers, morph_trait_size):
-        super(MorphTransformerModel, self).__init__()
+class LEMTransformerModel(nn.Module):
+    def __init__(self, num_chars, char_embedding_dim, num_filters, kernel_size, nhead, nhid, nlayers, lemma_size):
+        super(LEMTransformerModel, self).__init__()
         self.char_cnn_embedding = CharCNNEmbedding(num_chars, char_embedding_dim, num_filters, kernel_size)
         self.pos_encoder = PositionalEncoding(num_filters)
         transformer_layers = nn.TransformerEncoderLayer(num_filters, nhead, nhid, dropout=0.1)
         self.transformer_encoder = nn.TransformerEncoder(transformer_layers, nlayers)
-        self.hidden2morph = nn.Linear(num_filters, morph_trait_size)  
+        self.hidden2lem = nn.Linear(num_filters, lemma_size)
 
     def forward(self, sentence):
-        embeds = self.char_cnn_embedding(sentence)
+        embeds = self.char_cnn_embedding(sentence)  # Replace word embedding
         embeds = self.pos_encoder(embeds)
         transformer_out = self.transformer_encoder(embeds)
-        morph_trait_space = self.hidden2morph(transformer_out) 
-        morph_trait_scores = torch.log_softmax(morph_trait_space, dim=2)
-        return morph_trait_scores
-    
-#accuracy, evaluation
+        lem_space = self.hidden2lem(transformer_out)
+        lem_scores = torch.log_softmax(lem_space, dim=2)
+        return lem_scores
+
 def calculate_accuracy(true, predicted):
     correct = sum(t == p for t, p in zip(true, predicted))
     return correct / len(true) if len(true) > 0 else 0
 
-
-def evaluate_model(model, data_loader, loss_function,device, morph_to_ix):
+def evaluate_model(model, data_loader, loss_function,device, lemma_to_ix):
     model.eval()
     total_loss = 0
-    all_true_morph = []
-    all_predicted_morph = []
+    all_true_lem = []
+    all_predicted_lem = []
 
     with torch.no_grad():
         for sentence_in, targets in data_loader:
             sentence_in, targets = sentence_in.to(device), targets.to(device)
-            morph_scores = model(sentence_in)
-            loss = loss_function(morph_scores.view(-1, len(morph_to_ix)), targets.view(-1))
+            lem_scores = model(sentence_in)
+            loss = loss_function(lem_scores.view(-1, len(lemma_to_ix)), targets.view(-1))
             total_loss += loss.item()
 
             # Récupération des prédictions
-            predicted = torch.argmax(morph_scores, dim=2)
-            all_true_morph.extend(targets.flatten().tolist())
-            all_predicted_morph.extend(predicted.flatten().tolist())
+            predicted = torch.argmax(lem_scores, dim=2)
+            all_true_lem.extend(targets.flatten().tolist())
+            all_predicted_lem.extend(predicted.flatten().tolist())
 
     # Filtrer les étiquettes de padding
-    filtered_true_morph = [morph for morph in all_true_morph if morph != -1]
-    filtered_predicted_morph = [all_predicted_morph[i] for i, morph in enumerate(all_true_morph) if morph != -1]
+    filtered_true_lem = [lem for lem in all_true_lem if lem != -1]
+    filtered_predicted_lem = [all_predicted_lem[i] for i, lem in enumerate(all_true_lem) if lem != -1]
 
     # Calculer l'accuracy
-    accuracy = calculate_accuracy(filtered_true_morph, filtered_predicted_morph)
+    accuracy = calculate_accuracy(filtered_true_lem, filtered_predicted_lem)
     return total_loss / len(data_loader), accuracy
